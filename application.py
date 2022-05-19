aa2099093db1280a0b395e9ee62b472d5936057d
joselxes <fran3te@hotmail.com>
2022-05-19 18:03:55 -0500

    session added

--- application.py
+++ application.py
@@ -7,9 +7,8 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from models import *
 
 
-
 app = Flask(__name__)
-
+app.secret_key="hello"
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
@@ -41,8 +40,20 @@ def fComment(LReviews,RReview):
 
 @app.route("/",methods=["GET","POST"])
 def index():
-            
-    session["mensaje"]="j"
+    if not("mensaje" in session) :
+        session["mensaje"]="Bienvenido"
+    if "reader" in session:
+        if request.method == "POST":
+            session["session"] = request.form.get("session")
+            if session["session"] == "no":
+                session.pop('reader',None)
+                session["mensaje"]="ingresa o registrate"            
+                session["mensaje"]="session cerrada"
+        else:
+            return redirect(url_for('searchPage'))
+
+    # else:0
+
     return  render_template("trueLogin.html",mensaje=session["mensaje"])
 
 @app.route("/checklog",methods=["GET","POST"])
@@ -57,18 +68,21 @@ def checklog():
     # check if user is in users 
         print(db.execute("""SELECT * FROM "readers_id" """).fetchall())
         session["exists"]=db.execute("""SELECT * FROM "readers_id" WHERE "reader_id" = :reader""", {"reader":session["reader"] }).fetchone()
-        print(session["exists"])
+        print(session["exists"],session["register"])
         print(111111,session["register"],11111)
         if (session["exists"] is None) and session["register"]=="0":
-            print("puta mierda")
-            return redirect(url_for('index'))            
+            print("puta mierda", session["reader"] )
+            session.pop('reader',None)
+            session["mensaje"]="usuario no existe"
+            return redirect(url_for('index'))
         # print(session["exists"],"xxxxxxxx",session["exists"][0],session["exists"][0]==session["reader"])
 
-        if session["register"]=="0":
+        elif session["register"]=="0":
             if session["exists"]==(session["reader"],session["psw1"]):
                 print("user and pass correct")
                 return redirect(url_for('searchPage'))
             else:
+                session.pop('reader',None)
                 session["mensaje"]="contrasena o usuario, incorrecto"   
                 return redirect(url_for('index'))
 
@@ -147,6 +161,17 @@ def checkComment():
     #         print("error")
     # return render_template("error.html")+
 
+# @app.route("/endSession",methods=["GET","POST"])
+# def endSession():
+
+#     if request.method == "POST":
+#         # print("cerrar Session-----",session["reader"])
+#         session.clear()
+#         print("Session cerrarda -----\n")
+#         # return  render_template("trueLogin.html",mensaje="mensaje")
+#         return redirect(url_for('/'))
+#     else:
+#         return redirect(url_for('searchPage'))
 
 @app.route("/api/searchPage/<Source>" )
 def bookAPI(Source):
@@ -167,3 +192,6 @@ def bookAPI(Source):
     "review_count":session["data"]["books"][0]["work_ratings_count"],
     "average_score":session["data"]["books"][0]["average_rating"]
     })
+
+
+
